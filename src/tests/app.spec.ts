dd4f09e375d424fd053d2bfe73993732397ee8a1
Pham-Men <phamthimen0102@gmail.com>
2024-01-21 17:33:46 +0700

    21/1/2024

---
+++ src/tests/app.spec.ts
@@ -0,0 +1,11 @@
+import {Calculator} from "../app";
+
+describe("add function", () => {
+    test("add two numbers", () => {
+        const number1 = 1;
+        const number2 = 2;
+        const result = 3;
+
+        expect(Calculator.add(number1, number2)).toBe(result);
+    })
+})
